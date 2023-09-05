eac0003ee8b44361f14184ce2f63fdc86d4b70c0
Francisco de Asís Domínguez Iceta <ciscoadiz@gmail.com>
2023-09-05 12:13:03 +0100

    estado final

--- sales-cli/src/main/java/com/kineteco/SalesCli.java
+++ sales-cli/src/main/java/com/kineteco/SalesCli.java
@@ -1,4 +1,11 @@
 package com.kineteco;
 
-public class SalesCli  {
+import io.quarkus.runtime.QuarkusApplication;
+
+public class SalesCli implements QuarkusApplication {
+    @Override
+    public int run(String... args) throws Exception {
+        System.out.println("Hello " + args[0]);
+        return 0;
+    }
 }
