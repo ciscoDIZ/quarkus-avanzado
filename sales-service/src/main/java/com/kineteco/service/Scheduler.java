b1467ee95d6b6e8f3ff9be1b2a85e6c6f761f8a2
Francisco de Asís Domínguez Iceta <ciscoadiz@gmail.com>
2023-09-14 17:29:12 +0100

    estado final

--- sales-service/src/main/java/com/kineteco/service/Scheduler.java
+++ sales-service/src/main/java/com/kineteco/service/Scheduler.java
@@ -1,15 +1,29 @@
 package com.kineteco.service;
 
 import com.kineteco.model.Customer;
+import io.quarkus.runtime.configuration.ProfileManager;
+import io.quarkus.scheduler.Scheduled;
+import io.quarkus.scheduler.ScheduledExecution;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
+import java.util.concurrent.TimeUnit;
 
 @ApplicationScoped
 public class Scheduler {
    private static final Logger LOGGER = Logger.getLogger(Scheduler.class);
 
+   @Scheduled(every = "{kineteco.task}", skipExecutionIf = SkipIfNotDev.class)
    public void displayCustomerCount() {
      LOGGER.info(Customer.count());
    }
+}
+
+@Singleton
+class SkipIfNotDev implements Scheduled.SkipPredicate {
+    @Override
+    public boolean test(ScheduledExecution execution) {
+        return !"dev".equals(ProfileManager.getActiveProfile());
+    }
 }
